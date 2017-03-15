074dbb5f216e7ac03a2b5a9374d9530847686071
rafgardom <rafgardom2@alum.us.es>
2017-03-15 19:05:30 +0100

    Requisitos B

--- Acme-CnG/src/main/java/repositories/CommentRepository.java
+++ Acme-CnG/src/main/java/repositories/CommentRepository.java
@@ -1,11 +1,42 @@
+
 package repositories;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import domain.Actor;
 import domain.Comment;
 
 @Repository
-public interface CommentRepository extends JpaRepository<Comment, Integer>{
-	
+public interface CommentRepository extends JpaRepository<Comment, Integer> {
+
+	//Dashboard
+
+	//B1 a)
+	// Average number of comments per actor
+	@Query("select count(c)*1.0/(select count(a) from Actor a) from Comment c where c.commentableType = 'Customer' or c.commentableType = 'Administrator'")
+	public double commentsPerActor();
+
+	//B1 b)
+	// Average number of comments per offer
+	@Query("select count(c)*1.0/(select count(o) from Offer o) from Comment c where c.commentableType = 'Offer'")
+	public double commentsPerOffer();
+
+	//B1 c)
+	// Average number of comments per request
+	@Query("select count(c)*1.0/(select count(r) from Request r) from Comment c where c.commentableType = 'Request'")
+	public double commentsPerRequest();
+
+	//B2
+	// Average number of comments posted by administrators and customers
+	@Query("select avg(a.comments.size) from Actor a")
+	public double avgCommentsPerActor();
+
+	//B3
+	// The actors who have posted ±10% the average number of comments per actor
+	@Query("select a from Actor a where a.comments.size >= (select avg(a.comments.size)-0.1 from Actor a) and a.comments.size <= (select avg(a2.comments.size)+0.1 from Actor a2")
+	public Collection<Actor> actorsWhoHavePostThe10PercentMessages();
 }
--- Acme-CnG/src/main/java/services/CommentService.java
+++ Acme-CnG/src/main/java/services/CommentService.java
@@ -1,12 +1,17 @@
+
 package services;
 
 import java.util.Collection;
+import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
 
 import repositories.CommentRepository;
+import domain.Actor;
+import domain.Administrator;
 import domain.Comment;
 
 @Service
@@ -16,35 +21,99 @@ public class CommentService {
 	//Managed repository
 
 	@Autowired
-	private CommentRepository	commentRepository;
+	private CommentRepository		commentRepository;
 
 	//Supported services
+
+	@Autowired
+	private ActorService			actorService;
+
+	@Autowired
+	private AdministratorService	administratorService;
+
+
+	//Constructor
 
 	public CommentService() {
 		super();
 	}
 
-	public Comment create() {
-		return null;
+	public Comment create(final int commentableId, final String commentableType) {
+		final Actor actor = this.actorService.findByPrincipal();
+		Assert.notNull(actor);
+
+		final Comment result = new Comment();
+		result.setAuthor(actor);
+
+		result.setCommentableId(commentableId);
+		result.setCommentableType(commentableType);
+
+		return result;
 	}
 
 	public Collection<Comment> findAll() {
-		return commentRepository.findAll();
+		return this.commentRepository.findAll();
 	}
 
-	public Comment findOne(int commentId) {
-		return commentRepository.findOne(commentId);
+	public Comment findOne(final int commentId) {
+		return this.commentRepository.findOne(commentId);
 
 	}
 
-	public void save(Comment comment) {
-		commentRepository.save(comment);
+	public Comment save(final Comment comment) {
+		final Actor actor = this.actorService.findByPrincipal();
+		Assert.notNull(comment);
+		Comment result;
+
+		if (actor.getUserAccount().getAuthorities().iterator().next().equals("ADMINISTRATOR"))
+			result = this.save(comment);
+		else {
+			Assert.isTrue(comment.getAuthor().equals(actor));
+			Assert.isTrue(comment.getCommentableId() != actor.getId());
+			comment.setMoment(new Date(System.currentTimeMillis() - 1000));
+			comment.setBanned(false);
+			result = this.commentRepository.save(comment);
+		}
+
+		return result;
 	}
 
-	public void delete(Comment comment) {
-		commentRepository.delete(comment);
+	public void delete(final Comment comment) {
+
+		this.commentRepository.delete(comment);
 	}
 
 	//Other business methods
 
+	public void banComment(final Comment comment) {
+		Assert.notNull(comment);
+		final Administrator administrator = this.administratorService.findByPrincipal();
+		Assert.notNull(administrator);
+		Assert.isTrue(comment.isBanned() == false);
+
+		comment.setBanned(true);
+		this.save(comment);
+	}
+
+	//Dashboard
+
+	public double commentsPerActor() {
+		return this.commentRepository.commentsPerActor();
+	}
+
+	public double commentsPerOffer() {
+		return this.commentRepository.commentsPerOffer();
+	}
+
+	public double commentsPerRequest() {
+		return this.commentRepository.commentsPerRequest();
+	}
+
+	public double avgCommentsPerActor() {
+		return this.commentRepository.avgCommentsPerActor();
+	}
+
+	public Collection<Actor> actorsWhoHavePostThe10PercentMessages() {
+		return this.commentRepository.actorsWhoHavePostThe10PercentMessages();
+	}
 }
